8d9cda08910dff398e9f9fac085e2f62c1e9892e
haris49 <hariis.459@gmail.com>
2024-10-12 13:48:09 +0700

    commit 154

---
+++ app/src/main/java/com/example/pertemuan_2/TipeData.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan_2
+
+fun main(){
+    //byte
+    val byteValue: Byte = 10
+    println("byte: $byteValue")
+
+    //Short
+    val shortValue: Short = 20
+    println("short: $shortValue")
+
+    //long
+    val longValue: Long = 40
+    println("long: $longValue ")
+
+    //Float
+    val floatValue: Float =50.6f
+    println("float: $floatValue")
+
+    //double
+    val doubleValue: Double = 60.6
+    println("double: $doubleValue")
+
+    //char
+    val charValue: Char = 'A'
+    println("char: $charValue")
+
+    //boolean
+    val booleanValue: Boolean = true
+    println("boolean")
+
+    //string
+    val stringValue: String = "HELLO, WORLD!"
+    println("string $stringValue")
+}
